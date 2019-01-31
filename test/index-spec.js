052cbd44225e11b7715ee7b1ed7968abd12943a7
tatendamar <tatendamarufu05@gmail.com>
2019-01-31 10:32:26 +0200

    [finishes #163552973] testing my travis intergration

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,11 +1,11 @@
-const assert = require('assert');
-const calculator = require('../index');
+// const assert = require('assert');
+// const calculator = require('../index');
 
-describe('calculator', () => {
-  describe('add function', ()=> {
-    it('add numbers', () => {
-      let result = calculator.add(1, 1);
-      assert.equal(result, 2);
-    });
-  });
-});
+// describe('calculator', () => {
+//   describe('add function', ()=> {
+//     it('add numbers', () => {
+//       let result = calculator.add(1, 1);
+//       assert.equal(result, 2);
+//     });
+//   });
+// });
