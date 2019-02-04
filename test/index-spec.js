2e9874e4e7fe0cf25f6a24dc6148a54c588cab01
tatendamar <tatendamarufu05@gmail.com>
2019-02-04 14:45:42 +0200

    #163694783 seeting up coveralls

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,19 +1,11 @@
 // const assert = require('assert');
 // const calculator = require('../index');
 
-// describe('calculator', () => {
-//   describe('add function', ()=> {
-//     it('add numbers', () => {
-//       let result = calculator.add(1, 1);
-//       assert.equal(result, 2);
-//     });
-//   });
-// });
-
-let assert = require('assert');
-
-describe('Basic Mocha Test', () => {
-  it('should throw errors', () => {
-    assert.equal(3, 3);
+describe('calculator', () => {
+  describe('add function', () => {
+    it('add numbers', () => {
+      let result = calculator.add(1, 1);
+      assert.equal(result, 2);
+    });
   });
 });
