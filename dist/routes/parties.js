0525cf910ab0284c8ed924cd27808d717a60eee6
tatendamar <tatendamarufu05@gmail.com>
2019-02-14 15:45:58 +0200

    deleted the distb folder

--- dist/routes/parties.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var app = require('express')();
-
-var parties = require('../controllers/parties');
-
-app.route('/parties').get(parties.getParties).post(parties.postParty);
-app.route('/parties/:partyId').get(parties.getParty).put(parties.editParty).delete(parties.deleteParty);
-module.exports = app;
