3fa2e5f8c4dd5646351341a69ffe968ab8d35f3f
tatendamar <tatendamarufu05@gmail.com>
2019-02-15 00:27:06 +0200

    changes

--- dist/helpers/validateParty.js
+++ dist/helpers/validateParty.js
@@ -1,18 +1,30 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
 exports.default = void 0;
 
-var Joi = require('joi');
+var Joi = _interopRequireDefault(require('joi'));
 
 var validateParty = function validateParty(party) {
   var schema = {
-    name: Joi.string().min(2).max(30).required(),
-    email: Joi.string().email().required(),
-    address: Joi.string().alphanum().min(4).max(50).required(),
-    city: Joi.string().min(2).max(30).required(),
+    name: Joi.string()
+      .min(2)
+      .max(30)
+      .required(),
+    email: Joi.string()
+      .email()
+      .required(),
+    address: Joi.string()
+      .alphanum()
+      .min(4)
+      .max(50)
+      .required(),
+    city: Joi.string()
+      .min(2)
+      .max(30)
+      .required(),
     logo: Joi.string().required()
   };
   var options = {
